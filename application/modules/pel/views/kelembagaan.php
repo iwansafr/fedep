a1d9d29bc64f50f88a143581ea1726882ea13df9
iwansafr <iwansafr@gmail.com>
2021-08-18 10:53:18 +0700

    pdf on visi misi kelembagaan pel

--- application/modules/pel/views/kelembagaan.php
+++ application/modules/pel/views/kelembagaan.php
@@ -17,7 +17,8 @@ if(is_cluster())
 	$form->addInput('visi_misi','file');
 	// $form->setAttribute('visi_misi',['disabled'=>'disabled']);
 	$form->setLabel('visi_misi','Visi, Misi, Tujuan');
-	$form->setCaption('visi_misi','Visi, Misi, Tujuan Program dan Kegiatan (foto) (format gambar jpg dan png max 2mb)');
+	$form->setAccept('visi_misi','.pdf');
+	$form->setCaption('visi_misi','Visi, Misi, Tujuan Program dan Kegiatan (pdf) (format file pdf max 2mb)');
 	$form->addInput('dok_sk','file');
 	$form->setLabel('dok_sk','dokumentasi');
 	$form->setCaption('dok_sk','Dokumentasi surat keputusan sejak awal berdiri hingga saat ini yang menjelaskan tentang struktur organisasi, pembagian tugas tenaga kerja, aturan main pelaksanaan, jangka waktu kepengurusan (foto) (format gambar jpg dan png max 2mb)');
